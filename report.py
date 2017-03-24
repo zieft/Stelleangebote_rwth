e9d888db7e659f2c952dc8d7321389f006aeca40
Yulin Zhu <zieft@hotmail.com>
2017-03-24 15:01:29 +0100

    add fetch_all methed to gether all informations into a pandas DataFrame

--- report.py
+++ report.py
@@ -1,4 +1,7 @@
 from getdata import StelleRequest
+import pandas as pd
+
+
 
 URL = 'http://www.rwth-aachen.de/cms/root/Die-RWTH/Jobs-Ausbildung/~buym/Jobboerse/?search=&showall=1&aaaaaaaaaaaaanr=Studentische+Hilfskraft&aaaaaaaaaaaaans=&aaaaaaaaaaaaanv='
 
