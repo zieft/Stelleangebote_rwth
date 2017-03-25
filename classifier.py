ba85be0212ea4e993f2d9023cad069e01925ddbd
Yulin Zhu <zieft@hotmail.com>
2017-03-25 17:52:11 +0100

    classifier initializing

---
+++ classifier.py
@@ -0,0 +1,4 @@
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+import numpy as np
